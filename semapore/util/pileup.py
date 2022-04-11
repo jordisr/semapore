9d1716d927b988abc8b95578e624b021799c2390
Jordi Silvestre-Ryan <jordisr@users.noreply.github.com>
2022-04-11 03:18:57 -0700

    handle sparser pileups with multiple contigs

--- semapore/util/pileup.py
+++ semapore/util/pileup.py
@@ -1,101 +1,155 @@
 import re
+import sys
 import pysam
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from Bio import SeqIO
 
-class Pileup:
-    def __init__(self, alignment, reference):
-        self.alignment = alignment
-        self.reference = reference
-
-        self.pileup = get_pileup_alignment(alignment=self.alignment, reference=self.reference)
-        self.pileup = self.pileup.applymap(replace_tuple)
-
-        # assumes reference is first row
-        self.ref_id = self.pileup.columns[0]
-        self.reads = self.pileup.columns[1:]
-
-        self.n_columns = self.pileup.shape[0]
-        self.n_reads = self.pileup.shape[1] - 1
-
-def load_fastx(f, fmt="fasta"):
+def load_fastx_dict(f, fmt="fasta"):
     # read FASTA/FASTQ into memory
-    seqs = []
+    seqs = {}
     with open(f) as handle:
         for record in SeqIO.parse(handle, fmt):
-            seqs.append((record.id, str(record.seq)))
+            seqs[record.id] = str(record.seq)
     return seqs
 
-def get_pileup_alignment(alignment, reference):
+class ReadID:
+    def __init__(self):
+        self.n = -1
+        self.dict = {}
+        self.reads = []
+
+    def __getitem__(self, key):
+        read_id = self.dict.get(key, -1)
+        if read_id > 0:
+            return read_id
+        else:
+            self.n += 1
+            self.dict[key] = self.n
+            self.reads.append(key)
+            return self.n
+
+    def __len__(self):
+        return len(self.dict)
+
+class Pileup:
+    def __init__(self, alignment_path, reference_path, refname):
+        self.alignment_path = alignment_path
+        self.reference_path = reference_path
+        self.refname = refname
+        self.refseq = []
+        self.pileup = []
+        self.read_id = ReadID()
+
+    def add_columm(self, column):
+        self.pileup.append({})
+        for k,v in column.items():
+            if k == self.refname:
+                self.refseq.append(v)
+            else:
+                self.pileup[-1][self.read_id[k]] = v
+
+    def get_num_reads(self):
+        return self.read_id.n + 1
+
+    def get_num_columns(self):
+        return len(self.pileup)
+
+    def __len__(self):
+        return self.get_num_columns()
+
+    def get_window(self, start, end):
+        window = pd.DataFrame(self.pileup[start:end]).applymap(lambda x: (None,0) if pd.isnull(x) else x)
+        window_reads = [self.read_id.reads[x] for x in window.columns]
+        window_pos = window.applymap(lambda x: x[0]).to_numpy().astype(int)
+        window_pileup = window.applymap(lambda x: x[1]).to_numpy().astype(int)
+        window_refseq = np.array(self.refseq[start:end])[:,1]
+        return PileupWindow(reads=window_reads, refseq=window_refseq, pos=window_pos, pileup=window_pileup)
+
+    def get_reads(self):
+        return list(self.read_id.dict.keys())
+
+    def get_depth(self):
+        return np.array([len(x) for x in self.pileup])
+
+class PileupWindow:
+    def __init__(self, reads, refseq, pileup, pos):
+        self.reads = reads
+        self.pos = pos
+        self.pileup = pileup
+        self.refseq = refseq
+
+def get_pileup(alignment, reference):
     """Generate pileup alignment of reads aligned to the draft assembly/reference
 
     Args:
         alignment (str): path to BAM alignment
-        reference (str): path to draft assembly/reference in FASTA format
+        reference (str): path to alignment reference in FASTA format
 
     Returns:
-        DataFrame: one column for each sequence in alignment, one row for each position
+        List of Pileup objects
     """
 
-    (ref_id, ref_seq) = load_fastx(reference, "fasta")[0]
+    ref_seqs = load_fastx_dict(reference, "fasta")
 
-    del_marker = "<DEL>"
-    ins_marker = "<INS>"
-    no_marker = "<NONE>"
-
-    alignment_columns = []
-    pysam.set_verbosity(0)
-    with pysam.AlignmentFile(alignment, "rb") as samfile:
-
-        for pileupcolumn in samfile.pileup(min_base_quality=0):
-
-            #print(pileupcolumn.pos, pileupcolumn.n)
-            query_sequences = pileupcolumn.get_query_sequences(add_indels=True, mark_matches=True, mark_ends=False)
-            query_names = pileupcolumn.get_query_names()
-            query_positions = pileupcolumn.get_query_positions()
-            this_column = {ref_id:(pileupcolumn.pos, ref_seq[pileupcolumn.pos])}
-            insertions = {}
-            max_insert_size = 0
-
-            for base,read,pos in zip(query_sequences, query_names, query_positions):
-                if len(base) > 1:
-                    rem = re.match(r"([ACGTacgt])([-+])(\d+)([AaCcGgTtNn]+)",base)
-                    base_before, indel_type, indel_size, indel_seq = rem.groups(0)
-                    this_column[read] = (pos,base_before)
-
-                    # keep track of insertions to add columns to alignment later
-                    if indel_type == '+':
-                        insertions[read] = (pos,indel_seq)
-                        if int(indel_size) > max_insert_size:
-                            max_insert_size = int(indel_size)
-
-                elif base == '*':
-                    this_column[read] = del_marker
-                else:
-                    this_column[read] = (pos, base)
-
-            alignment_columns.append(this_column)
-
-            # add insertion columns
-            for i in range(max_insert_size):
-                insertion_col = {ref_id:ins_marker}
-                for k,v in insertions.items():
-                    if i < len(v[1]):
-                        insertion_col[k] = (v[0]+1+i, v[1][i])
-                    else:
-                        insertion_col[k] = ins_marker
-                for r in query_names:
-                    if r not in insertions:
-                        insertion_col[r] = ins_marker
-                alignment_columns.append(insertion_col)
-
-    return pd.DataFrame(alignment_columns).fillna(no_marker)
-
-def replace_tuple(x):
     char_encoding = {v:i for i,v in enumerate(['<NONE>','<DEL>','<INS>','A','C','G','T','a','c','g','t'])}
+    
+    sequence_pileups = []
+    pysam.set_verbosity(0)
+    
+    with pysam.AlignmentFile(alignment, "rb") as samfile:
+        with tqdm(total=len(ref_seqs)) as pbar:
+            pileupref = None
+            for pileupcolumn in samfile.pileup(min_base_quality=0):
+                if pileupcolumn.reference_name != pileupref:
+                    if pileupref:
+                        sequence_pileups.append(curr_pileup)
+                    pileupref = pileupcolumn.reference_name                    
+                    curr_pileup = Pileup(alignment_path=alignment, reference_path=reference, refname=pileupref)
+                    pbar.update(1)
 
-    if type(x) is str:
-        return (-1, char_encoding[x])
-    else:
-        return (x[0], char_encoding[x[1]])
+                query_sequences = pileupcolumn.get_query_sequences(add_indels=True, mark_matches=True, mark_ends=False)
+                query_names = pileupcolumn.get_query_names()
+                query_positions = pileupcolumn.get_query_positions()
+
+                this_column = {pileupref:(pileupcolumn.pos, char_encoding[ref_seqs[pileupref][pileupcolumn.pos]])}
+                insertions = {}
+                max_insert_size = 0
+
+                for base,read,pos in zip(query_sequences, query_names, query_positions):
+                    if len(base) > 1:
+                        re_match = re.match(r"([ACGTacgt])([-+])(\d+)([AaCcGgTtNn]+)",base)
+                        base_before, indel_type, indel_size, indel_seq = re_match.groups(0)
+                        this_column[read] = (pos,char_encoding[base_before])
+
+                        # keep track of insertions to add columns to alignment later
+                        if indel_type == '+':
+                            insertions[read] = (pos,indel_seq)
+                            if int(indel_size) > max_insert_size:
+                                max_insert_size = int(indel_size)
+
+                    elif base == '*':
+                        this_column[read] = (None, char_encoding['<DEL>'])
+
+                    else:
+                        this_column[read] = (pos, char_encoding[base])
+
+                curr_pileup.add_columm(this_column)
+
+                # add insertion columns
+                for i in range(max_insert_size):
+                    insertion_col = {pileupref:(None, char_encoding['<INS>'])}
+                    for k,v in insertions.items():
+                        if i < len(v[1]):
+                            insertion_col[k] = (v[0]+1+i, char_encoding[v[1][i]])
+                        else:
+                            insertion_col[k] = (None, char_encoding['<INS>'])
+                    for r in query_names:
+                        if r not in insertions:
+                            insertion_col[r] = (None, char_encoding['<INS>'])
+                    curr_pileup.add_columm(insertion_col)
+
+        sequence_pileups.append(curr_pileup)
+
+    return sequence_pileups
--- semapore/util/preprocessing.py
+++ semapore/util/preprocessing.py
@@ -2,112 +2,17 @@ from math import ceil
 import sys
 import numpy as np
 
-def featurize_inputs(pileup, reads, window_size=100, max_time=150, trim_down=False, draft_first=False):
-    """Generate padded input features from the raw signal and reads-to-assembly alignment
-
-    Args:
-        pileup (Pileup)
-        reads (dict): output of get_fast5_reads()
-        window_size (int): number of pileup alignment columns per input
-        max_time (int): max signal/event size, larger ones will be truncated
-        trim_down (bool): whether to reduce signal array if all events < max_time
-
-    Returns:
-        ndarray: alignment
-            [?, window_size, num_reads]
-        ndarray: signal
-            [?, window_size, num_reads, max_time]
-        ndarray: signal_masks
-            [?, window_size, num_reads, max_time]
-        ndarray: window_bounds
-            [?, 2]
-    """
-
-    # TODO: dynamically resize if it exceeds?
-    # TODO: add in padding for last incomplete batch
-    # using total number of reads is possibly impractical for genome-level assemblies
-    signal_data = np.zeros((pileup.n_columns // window_size, window_size, pileup.n_reads, max_time), dtype=np.float32)
-    signal_mask = np.zeros((pileup.n_columns // window_size, window_size, pileup.n_reads, max_time), dtype=bool)
-    window_bounds = np.zeros((pileup.n_columns // window_size, 2), dtype=np.int32)
-    sequence_data = []
-
-    # same padding across all windows, saves hassle of nested RaggedTensor
-    max_signals = 0
-
-    col = 0
-    col_i = 0
-
-    num_signals_processed = 0
-    num_over_maxtime = 0
-    while col+window_size < pileup.n_columns:
-        sequence_cols = []
-
-        pileup_window = pileup.pileup.iloc[col:col+window_size].to_numpy()
-
-        # can we use more vectorization?
-        for i in range(window_size):
-            alignment_column = np.array([x[1] for x in pileup_window[i]])
-            sequence_cols.append(alignment_column)
-            alignment_idx = np.array([x[0] for x in pileup_window[i]])
-
-            for j, read_id in enumerate(pileup.reads):
-                if alignment_column[j+draft_first] > 2: # A,C,G,T,a,c,g,t
-
-                    # alignment index => basecall sequence index
-                    basecall_idx = alignment_idx[j+draft_first]
-                    if alignment_column[j+draft_first] > 6:
-                        # correct position if sequence is reverse mapped
-                        basecall_idx = reads[read_id]["segments"].shape[0] - basecall_idx - 1
-
-                    # basecall sequence index => signal event bounds
-                    event_bounds = reads[read_id]["segments"][basecall_idx]
-                    #base_from_read = reads[read_id]["sequence"][basecall_idx]
-                    #base_from_pileup = ['A','C','G','T','a','c','g','t'][(alignment_column[j+1]-3)]
-                    #print(j, base_from_read, base_from_pileup)
-
-                    # signal event bounds => raw signal
-                    this_signal = reads[read_id]["signal"][event_bounds[0]:event_bounds[1]]
-                    num_signals = this_signal.shape[0]
-
-                    signal_data[col_i, i, j, :num_signals] = this_signal[:max_time]
-                    signal_mask[col_i, i, j, :num_signals] = True
-
-                    num_signals_processed += 1
-                    if num_signals > max_time:
-                        # track truncated events
-                        num_over_maxtime += 1
-                    elif num_signals > max_signals:
-                        max_signals = num_signals
-
-                else:
-                    this_signal = []
-
-        window_bounds[col_i] = np.array([col, col+window_size])
-        col += window_size
-        col_i += 1
-
-        sequence_data.append(sequence_cols)
-
-    if not trim_down:
-        max_signals = max_time
-
-    #print("{}% events truncated (max_time={})".format(format(num_over_maxtime/num_signals_processed*100, ".2f"), max_time))
-    return np.array(sequence_data), signal_data[:,:,:,:max_signals], signal_mask[:,:,:,:max_signals], window_bounds
-
-def featurize_inputs_ragged(pileup, reads, window_size=100, max_time=150, draft_first=True, drop_remainder=False):
+def featurize_inputs_ragged(pileup, reads, window_size=100, max_time=150, drop_remainder=False):
     # returns list of examples
     # each example described by dictionary with the following keys:
-    # 'signal_values','signal_row_lengths','signal_col_lengths','sequence_values','sequence_col_lengths'
+    # 'signal_values','signal_row_lengths','signal_col_lengths','sequence_values','sequence_col_lengths', 'draft'
     # flat values for constructing ragged tensors
     # either to be serialized or passed to a generator
 
+    num_columns = pileup.get_num_columns()
     if drop_remainder:
-        print((pileup.n_columns // window_size)*window_size, pileup.n_columns)
-        num_columns = (pileup.n_columns // window_size)*window_size
-    else:
-        num_columns = pileup.n_columns
+        num_columns = (num_columns // window_size)*window_size
 
-    # num_reads is total number of reads, possibly impractical for large assemblies
     window_bounds = np.zeros((ceil(num_columns / window_size), 2), dtype=np.int32)
     examples = []
 
@@ -116,6 +21,7 @@ def featurize_inputs_ragged(pileup, reads, window_size=100, max_time=150, draft_
 
     num_signals_processed = 0
     num_over_maxtime = 0
+
     while col < num_columns:
         this_window_size = min(col+window_size, num_columns)-col
         
@@ -126,23 +32,23 @@ def featurize_inputs_ragged(pileup, reads, window_size=100, max_time=150, draft_
         sequence_col_lengths = []
         draft = []
 
-        pileup_window = pileup.pileup.iloc[col:col+window_size].to_numpy()
+        pileup_window = pileup.get_window(col, col+window_size)
 
-        # can we use more vectorization?
         for i in range(this_window_size):
-            alignment_column = np.array([x[1] for x in pileup_window[i]])
+            alignment_column = pileup_window.pileup[i]
+            alignment_idx = pileup_window.pos[i]
+
             draft.append(alignment_column[0])
             sequence_values.append(alignment_column[1:])
             sequence_col_lengths.append(len(alignment_column)-1)
-            alignment_idx = np.array([x[0] for x in pileup_window[i]])
-
+           
             n_signal = 0
-            for j, read_id in enumerate(pileup.reads):
-                if alignment_column[j+draft_first] > 2: # A,C,G,T,a,c,g,t
+            for j, read_id in enumerate(pileup_window.reads):
+                if alignment_column[j] > 2: # A,C,G,T,a,c,g,t
                     n_signal += 1
                     # alignment index => basecall sequence index
-                    basecall_idx = alignment_idx[j+draft_first]
-                    if alignment_column[j+draft_first] > 6:
+                    basecall_idx = alignment_idx[j]
+                    if alignment_column[j] > 6:
                         # correct position if sequence is reverse mapped
                         basecall_idx = reads[read_id]["segments"].shape[0] - basecall_idx - 1
 
